fcb24620104f8a7c4e25b941b23d8a1b0d5102d2
as <>
2021-01-06 06:11:17 +0900

    名前の表示

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,12 +6,7 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            //数値の表示
-            Console.Write(123);
-            Console.WriteLine(456);
-            //文字列の表示
-            Console.Write("ABC");
-            Console.WriteLine("DEF");
+            Console.WriteLine("atsu");
         }
     }
 }
