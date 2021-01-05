b003aa362cca6812cd7be7ab831a035f97dd239f
as <>
2021-01-06 06:57:40 +0900

    変数について

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,10 +6,17 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} + {1} = {2}", 5, 2, 5 + 2);  //足し算
-            Console.WriteLine("{0} - {1} = {2}", 5, 2, 5 - 2);  //引き算
-            Console.WriteLine("{0} * {1} = {2}", 5, 2, 5 * 2);  //掛け算
-            Console.WriteLine("{0} / {1} = {2} 余り {3}", 5, 2, 5 / 2, 5 % 2);  //割り算・剰余
+            int a;          //変数の宣言
+            int b = 3;      //初期化と代入を同時に行う
+            int add, sub;   //複数の変数を同時に宣言
+            double avg;     //int以外の変数を宣言
+            a = 6;          //代入　（最初に値を入れるので”初期化”と言う
+            add = a + b;    //a, bの和を求める
+            sub = a - b;    //a, bの差を求める
+            avg = (a + b) / 2.0;    //a, bの平均値を求める
+            Console.WriteLine("{0} + {1} = {2}", a, b, add);
+            Console.WriteLine("{0} - {1} = {2}", a, b, sub);
+            Console.WriteLine("{0}と{1}の平均値は{2}", a, b, avg);
         }
     }
 }
