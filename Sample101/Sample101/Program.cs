4637526d7484705d586188100fff8cd8e4e4ea8a
as <>
2021-01-06 17:53:28 +0900

    条件分岐 ifについて

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,8 +6,16 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            string s1 = "あいう", s2 = "えお", s3 = s1 + s2;
-            Console.WriteLine(s3);
+            //キーワードから数値を入力
+            Console.Write("整数値を入力:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("a=" + a);
+
+            //入力した値が、正の数かどうか調べる
+            if (a > 0)
+            {
+                Console.WriteLine("aは正の数です。");  //正の数だった場合に実行
+            }
         }
     }
 }
