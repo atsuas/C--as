20e04b29a65a690c149101a0341565eef3d4bba0
as <>
2021-01-07 07:13:16 +0900

    if文の練習

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,24 +6,20 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            Console.Write("サイコロの目(1~6):");
-            int dice = int.Parse(Console.ReadLine());
+            Console.Write("tempの数値は:");
+            double temp = double.Parse(Console.ReadLine());
 
-            switch (dice)
+            if (temp == 0.0)
             {
-                case 2:
-                case 4:
-                case 6:
-                    Console.WriteLine("丁です！");
-                    break;
-                case 1:
-                case 3:
-                case 5:
-                    Console.WriteLine("半です！");
-                    break;
-                default:
-                    Console.WriteLine("範囲外の値です");
-                    break;
+                Console.WriteLine("ゼロ");
+            }
+            else if (0.0 < temp)
+            {
+                Console.WriteLine("プラス");
+            }
+            else
+            {
+                Console.WriteLine("マイナス");
             }
         }
     }
