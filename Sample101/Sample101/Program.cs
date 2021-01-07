03fa9411a1da34d780362d7dc1f5605293359326
as <>
2021-01-07 14:50:55 +0900

    for文のネストについて

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,11 +6,15 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            for (int i = 10; i >= 1; i--)
+            for (int i = 1; i <= 2; i++)
             {
-                Console.Write(i + " ");
+                for (int j = 1; j <= 3; j++)
+                {
+                    int k = i + j;
+                    Console.Write(i + "+" + j + "=" + k + " ");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
         }
     }
 }
