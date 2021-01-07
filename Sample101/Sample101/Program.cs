31591eb980b03a16a570503e28fb291600374384
as <>
2021-01-08 08:56:15 +0900

    配列変数について

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,25 +6,16 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int a = rnd.Next(1, 11);    //1から10までの乱数を発生させaに代入する
-            int b = rnd.Next(1, 11);    //1から10までの乱数を発生させbに代入する
-            Console.WriteLine("a={0} b={1}", a, b);
-            //  回答を入力（正解が出るまで繰り返す）
-            while (true)
-            {
-                Console.Write("a+b=");
-                int ans = int.Parse(Console.ReadLine());    //回答の数値を入力
-                if (ans == a + b)
-                {
-                    Console.WriteLine("正解");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("間違い");
-                }
-            }
+            double one, two, three;
+            double sum, avg;
+            one = 1.2;
+            two = 3.7;
+            three = 4.1;
+            Console.WriteLine(one + " " + two + " " + three);
+            sum = one + two + three;
+            avg = sum / 3.0;
+            Console.WriteLine("合計値:" + sum);
+            Console.WriteLine("平均値:" + avg);
         }
     }
 }
