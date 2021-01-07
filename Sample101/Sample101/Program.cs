2f1d981b479c7a5d20961f4e5922fbcf592895d9
as <>
2021-01-07 17:58:33 +0900

    1から10までの乱数を発生させて計算結果を出す・練習③

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,17 +6,25 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            Console.Write("整数を入力:");
-            int num = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= num; i++)
+            Random rnd = new Random();
+            int a = rnd.Next(1, 11);    //1から10までの乱数を発生させaに代入する
+            int b = rnd.Next(1, 11);    //1から10までの乱数を発生させbに代入する
+            Console.WriteLine("a={0} b={1}", a, b);
+            //  回答を入力（正解が出るまで繰り返す）
+            while (true)
             {
-                if (num % i == 0)
+                Console.Write("a+b=");
+                int ans = int.Parse(Console.ReadLine());    //回答の数値を入力
+                if (ans == a + b)
                 {
-                    Console.Write("{0} ", i);
+                    Console.WriteLine("正解");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("間違い");
                 }
             }
-
         }
     }
 }
