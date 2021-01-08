2449175117e63cc03e5fbd336bcd5e71122d54e4
as <>
2021-01-08 09:15:08 +0900

    配列変数について②

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,14 +6,19 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            double one, two, three;
-            double sum, avg;
-            one = 1.2;
-            two = 3.7;
-            three = 4.1;
-            Console.WriteLine(one + " " + two + " " + three);
-            sum = one + two + three;
-            avg = sum / 3.0;
+            double[] d = new double[3];
+            d[0] = 1.2;
+            d[1] = 3.7;
+            d[2] = 4.1;     //変数の代入
+            double sum, avg;    //合計値、平均値を入れる変数
+            sum = 0.0;
+            for (int i = 0; i < d.Length; i++)
+            {
+                Console.Write(d[i] + " ");
+                sum += d[i];
+            }
+            Console.WriteLine();
+            avg = sum / d.Length;
             Console.WriteLine("合計値:" + sum);
             Console.WriteLine("平均値:" + avg);
         }
