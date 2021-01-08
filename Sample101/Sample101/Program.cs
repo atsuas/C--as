22405e6c19d63e5193886668eb55beefed61c440
as <>
2021-01-09 07:48:15 +0900

    アクセス指定子とプロパティ

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,28 +6,15 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            Vecter2D v1 = new Vecter2D();
-            v1.x = 1.0; v1.y = 1.0;
-            Console.WriteLine("v1=({0}, {1})", v1.x, v1.y); //  v1=（1,1）と表示される
-
-            Vecter2D v2 = new Vecter2D();
-            v2.x = 1.0; v2.y = -1.0;
-            Console.WriteLine("v2=({0}, {1})", v2.x, v2.y);   //  v2= (1,-1) と表示される
-
-            //v1+v2
-            v1.Add(v2);
-            Console.WriteLine("v1=({0}, {1})", v1.x, v1.y); //  v1=（2,0）と表示される
-
-            //v1-v2
-            v1.Sub(v2);
-            Console.WriteLine("v1=({0}, {1})", v1.x, v1.y); //  v1=（1,1）と表示される
-
-            //2*v1
-            v1.Mul(2.0);
-            Console.WriteLine("v1=({0}, {1})", v1.x, v1.y); //  v1=（2,2）と表示される
-
-            //v1.v2の内積
-            Console.WriteLine("v1, v2の内積: {0}", v1.DotProduct(v2)); //v1.v1の内積は０と出る
+            Person p1, p2;
+            p1 = new Person();  //一つ目のPersonクラスのメソッドのインスタンスを生成
+            p2 = new Person();  //二つ目のPersonクラスのメソッドのインスタンスを生成
+            p1.Name = "山田太郎";   //フィールドnameに値を代入
+            p1.Age = 19;            //フィールドageに値を代入
+            p2.SetAgeAndName("佐藤花子", 23);   //setAgeAndName()メソッドで、nameとageを設定
+            p1.ShowAgeAndName();            //メソッドから名前と年齢を表示
+            //プロパティから名前と年齢を表示
+            Console.WriteLine("名前: {0} 年齢: {1}", p2.Name, p2.Age);
         }
     }
 }
