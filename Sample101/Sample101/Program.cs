86456ff6fb29819a9a44ea15a2e77c69d544392a
as <>
2021-01-08 09:59:02 +0900

    多次元配列（ 二次元配列について ）

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,12 +6,25 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            int[] n = { 1, 2, 3, 4 };
-            foreach (int i in n)
+            int[,] a = new int[3, 4];
+            int m, n;
+            //　二次元配列に値を代入
+            for (m = 0; m < 3; m++)
             {
-                Console.Write("{0} ", i);
+                for (n = 0; n < 4; n++)
+                {
+                    a[m, n] = m + n;
+                }
             }
-            Console.WriteLine();
+            //　二次元配列に値を出力
+            for (m = 0; m < 3; m++)
+            {
+                for (n = 0; n < 4; n++)
+                {
+                    Console.Write("a[{0}, {1}]={2}", m, n, a[m, n]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
