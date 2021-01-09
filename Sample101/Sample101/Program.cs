f50397641eb81c32c8c2a025c2b925740ed130a5
as <>
2021-01-09 14:56:34 +0900

    コンストラクタ・練習

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,7 +6,26 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            Dummy d = new Dummy();
+            //追加部分 ↓
+            Vector2D v1 = new Vector2D(1.0, 1.0);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+            Vector2D w = new Vector2D(1.0, -1.0);
+            Vector2D v2 = new Vector2D(w);
+            //追加部分 ↑
+
+            Console.WriteLine("v2=({0}, {1})", v2.X, v2.Y);
+
+            v1.Add(v2);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+
+            v1.Sub(v2);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+
+            v1.Mul(2.0);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+
+            Console.WriteLine("v1, v2の内積:{0}", v1.DotProduct(v2));
+
         }
     }
 }
--- Sample101/Sample101/Vector2D.cs
+++ Sample101/Sample101/Vector2D.cs
@@ -5,6 +5,44 @@ namespace Sample101
 {
     class Vector2D
     {
-        
+        //追加部分 ↓
+        public Vector2D()
+        {
+
+        }
+        public Vector2D(double x,double y)
+        {
+            X = x; Y = y;
+        }
+        public Vector2D(Vector2D v)
+        {
+            X = v.X; Y = v.Y;
+        }
+        //追加部分↑
+
+        public void Add(Vector2D v)
+        {
+            X += v.X; Y += v.Y; 
+        }
+        public void Sub(Vector2D v)
+        {
+            X -= v.X; Y -= v.Y;
+        }
+        public void Mul(double k)
+        {
+            X *= k; Y *= k;
+        }
+        public double DotProduct(Vector2D v)
+        {
+            return X * v.X + Y * v.Y;
+        }
+        public double X
+        {
+            set; get;
+        }
+        public double Y
+        {
+            set; get;
+        }
     }
 }
