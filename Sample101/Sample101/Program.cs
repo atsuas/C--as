c347b0570d00aee050073b6269235bffdc4d1454
as <>
2021-01-09 10:32:03 +0900

    コンストラクタについて

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,29 +6,13 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            Vector2D v1 = new Vector2D();
-            v1.X = 1.0; v1.Y = 1.0;
-            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y); //  v1=（1,1）と表示される
-
-            Vector2D v2 = new Vector2D();
-            v2.X = 1.0; v2.Y = -1.0;
-            Console.WriteLine("v2=({0}, {1})", v2.X, v2.Y);   //  v2= (1,-1) と表示される
-
-            //v1+v2
-            v1.Add(v2);
-            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y); //  v1=（2,0）と表示される
-
-            //v1-v2
-            v1.Sub(v2);
-            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y); //  v1=（1,1）と表示される
-
-            //2*v1
-            v1.Mul(2.0);
-            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y); //  v1=（2,2）と表示される
-
-            //v1.v2の内積
-            Console.WriteLine("v1, v2の内積: {0}", v1.DotProduct(v2)); //v1.v1の内積は０と出る
-
+            Person p1, p2;
+            p1 = new Person();  //引数なしのコンストラクタ
+            p2 = new Person("太田たかし", 29);   //引数ありのコンストラクタ
+            p1.Name = "斎藤花子";
+            p1.Age = 18;
+            p1.ShowAgeAndName();
+            p2.ShowAgeAndName();
         }
     }
 }
