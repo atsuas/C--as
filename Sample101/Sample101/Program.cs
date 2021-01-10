14a31172a0505599a533a9cd5490c52c2ea1400e
as <>
2021-01-10 11:30:13 +0900

    静的メンバ・Mainメソッドの練習① Vector２Dの作成

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -4,27 +4,29 @@ namespace Sample101
 {
     class Program
     {
-        //staticなフィールド
-        private static int snum = 100;
-        //インスタンスフィールド
-        public int inum = 200;
-        //staticなメソッド
-        public static void foo()
-        {
-            Console.WriteLine("fooメソッド　(staticメソッド)");
-        }
-        public void bar()
-        {
-            Console.WriteLine("barメソッド(インスタンスメソッド)");
-        }
         static void Main(string[] args)
         {
-            //インスタンスの生成
-            Program p = new Program();
-            Console.WriteLine("pのインスタンスフィールド:inum = {0}", p.inum);
-            Console.WriteLine("Programのクラスフィールド:snum = {0}", snum);
-            foo();
-            p.bar();
+
+            //追加部分 ↓
+            Vector2D v1 = new Vector2D(1.0, 1.0);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+            Vector2D w = new Vector2D(1.0, -1.0);
+            Vector2D v2 = new Vector2D(w);
+            //追加部分 ↑
+
+            Console.WriteLine("v2=({0}, {1})", v2.X, v2.Y);
+
+            v1.Add(v2);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+
+            v1.Sub(v2);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+
+            v1.Mul(2.0);
+            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+
+            Console.WriteLine("v1, v2の内積:{0}", v1.DotProduct(v2));
+
         }
     }
 }
