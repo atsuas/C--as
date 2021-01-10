62b2acc2832baa54426a802e3844c094f54b6d5d
as <>
2021-01-10 11:41:57 +0900

    静的メンバ・Mainメソッドの練習①

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -4,26 +4,30 @@ namespace Sample101
 {
     class Program
     {
+        //ベクトルの内容を表示するメソッド
+        static void Show(string name, Vector2D v)
+        {
+            Console.WriteLine("{0}=({1}, {2})", name,v.X, v.Y);
+        }
+
         static void Main(string[] args)
         {
-
-            //追加部分 ↓
+            
             Vector2D v1 = new Vector2D(1.0, 1.0);
-            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+            Show("v1", v1);     //  v1=(1,1)と表示
             Vector2D w = new Vector2D(1.0, -1.0);
-            Vector2D v2 = new Vector2D(w);
-            //追加部分 ↑
 
-            Console.WriteLine("v2=({0}, {1})", v2.X, v2.Y);
+            Vector2D v2 = new Vector2D(w);
+            Show("v2", v2);     //  v2=(1,-1)と表示
 
             v1.Add(v2);
-            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+            Show("v1", v1);
 
             v1.Sub(v2);
-            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+            Show("v1", v1);
 
             v1.Mul(2.0);
-            Console.WriteLine("v1=({0}, {1})", v1.X, v1.Y);
+            Show("v1", v1);
 
             Console.WriteLine("v1, v2の内積:{0}", v1.DotProduct(v2));
 
