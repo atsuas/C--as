4ae96c55ea4e11096628755985c9c3ffc8490fab
as <>
2021-01-10 13:19:11 +0900

    継承について

---
+++ Sample101/Sample101/ExCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Sample101
+{
+    //Calculatorクラスを継承した、ExCalculatorクラス
+    class ExCalculator : Calculator
+    {
+        //掛け算
+        public void mul ()
+        {
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+        }
+        //割り算
+        public void div ()
+        {
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+        }
+    }
+}
--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -7,19 +7,23 @@ namespace Sample101
         
         static void Main(string[] args)
         {
-            Console.WriteLine("実数値を入力してください。");
-            double n = double.Parse(Console.ReadLine());
-            if(n >= 0)
-            {
-                //  平方根の表示
-                double r = Math.Sqrt(n);    //  nの平方根をrに代入
-                Console.WriteLine("{0}の平方根は{1}です。", n, r);
-            }
-            else
-            {
-                //  負の値の場合
-                Console.WriteLine("正の数を入力してください。");
-            }
+            //Calculatorクラスのインスタンス
+            Calculator c1 = new Calculator();
+            c1.Num1 = 10;
+            c1.Num2 = 3;
+            //足し算・引き算の結果を表示
+            c1.add();
+            c1.sub();
+
+            ExCalculator c2 = new ExCalculator();
+            c2.Num1 = 10;
+            c2.Num2 = 3;
+            //足し算・引き算の結果を表示
+            c2.add();
+            c2.sub();
+            //掛け算・割り算の結果を表示
+            c2.mul();
+            c2.div();
         }
     }
 }
