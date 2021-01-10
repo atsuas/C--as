720d0b25ef886a95ebb2b671702ce7cfcd8e8819
as <>
2021-01-10 13:22:32 +0900

    Superクラス・Subクラスの作成

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -7,23 +7,7 @@ namespace Sample101
         
         static void Main(string[] args)
         {
-            //Calculatorクラスのインスタンス
-            Calculator c1 = new Calculator();
-            c1.Num1 = 10;
-            c1.Num2 = 3;
-            //足し算・引き算の結果を表示
-            c1.add();
-            c1.sub();
-
-            ExCalculator c2 = new ExCalculator();
-            c2.Num1 = 10;
-            c2.Num2 = 3;
-            //足し算・引き算の結果を表示
-            c2.add();
-            c2.sub();
-            //掛け算・割り算の結果を表示
-            c2.mul();
-            c2.div();
+            
         }
     }
 }
