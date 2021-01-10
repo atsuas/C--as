8509605a78345f2a8eedaa28efda4ef181117b4c
as <>
2021-01-10 11:55:33 +0900

    静的メンバ・Mainメソッドの練習② 二つのうちの最大値を表示する

--- Sample101/Sample101/Numeric.cs
+++ Sample101/Sample101/Numeric.cs
@@ -5,6 +5,13 @@ namespace Sample101
 {
     class Numeric
     {
-        
+        public static int Max(int a, int b)
+        {
+            if(a > b)
+            {
+                return a;
+            }
+            return b;
+        }
     }
 }
--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -4,33 +4,15 @@ namespace Sample101
 {
     class Program
     {
-        //ベクトルの内容を表示するメソッド
-        static void Show(string name, Vector2D v)
-        {
-            Console.WriteLine("{0}=({1}, {2})", name,v.X, v.Y);
-        }
-
+        
         static void Main(string[] args)
         {
-            
-            Vector2D v1 = new Vector2D(1.0, 1.0);
-            Show("v1", v1);     //  v1=(1,1)と表示
-            Vector2D w = new Vector2D(1.0, -1.0);
-
-            Vector2D v2 = new Vector2D(w);
-            Show("v2", v2);     //  v2=(1,-1)と表示
-
-            v1.Add(v2);
-            Show("v1", v1);
-
-            v1.Sub(v2);
-            Show("v1", v1);
-
-            v1.Mul(2.0);
-            Show("v1", v1);
-
-            Console.WriteLine("v1, v2の内積:{0}", v1.DotProduct(v2));
-
+            Console.Write("一つ目の数:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("二つ目の数:");
+            int b = int.Parse(Console.ReadLine());
+            int n = Numeric.Max(a, b);  //  nにa, bのうち最大値を代入
+            Console.WriteLine("二つの数値のうち最大値: {0}", n);
         }
     }
 }
