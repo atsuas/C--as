f293720f9b9ba854641b516fb464d90d4898518c
as <>
2021-01-12 08:10:30 +0900

    Listの練習①

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;   //コレクションを使う際に記述する
+using System.Text;
+using System.Linq;                  //Max、Minを表示する際に記述が必要
+using System.Threading.Tasks;
 
 namespace Sample101
 {
@@ -7,18 +10,25 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            //ハッシュセットの生成
-            HashSet<int> t = new HashSet<int>();
-            //データの追加
-            t.Add(1);
-            t.Add(2);
-            t.Add(3);
-            t.Add(1);   //被っているので出力されない
-            //データの出力
-            foreach (int i in t)
+            List<int> l = new List<int>();
+            while (true)
             {
-                Console.WriteLine("{0}", i);
+                Console.Write("1~10の整数を入力:");
+                int num = int.Parse(Console.ReadLine());
+                if(num < 1 || num > 10)
+                {
+                    break;  //範囲外であればループから抜ける
+                }
+                l.Add(num);
             }
+
+            foreach(int n in l)
+            {
+                Console.Write("{0} ", n);
+            }
+            Console.WriteLine();
+            //　最大値・最小値の表示
+            Console.WriteLine("最大値{0} 最小値{1}", l.Max(), l.Min());
         }
     }
 }
