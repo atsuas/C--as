319f69178b9d7e98a621ceb2e7f2a06b25045871
as <>
2021-01-12 07:36:34 +0900

    HashSetについて

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -7,16 +7,17 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            Dictionary<String, String> capital = new Dictionary<string, string>();
-            //連想記憶クラスの生成
-            capital["日本"] = "東京";
-            capital["イギリス"] = "ロンドン";
-            capital["フランス"] = "パリ";
-            capital["中国"] = "北京";
-            Console.WriteLine("世界の首都");
-            foreach (String s in capital.Keys)
+            //ハッシュセットの生成
+            HashSet<int> t = new HashSet<int>();
+            //データの追加
+            t.Add(1);
+            t.Add(2);
+            t.Add(3);
+            t.Add(1);   //被っているので出力されない
+            //データの出力
+            foreach (int i in t)
             {
-                Console.WriteLine("{0}の首都は{1}です", s, capital[s]);
+                Console.WriteLine("{0}", i);
             }
         }
     }
