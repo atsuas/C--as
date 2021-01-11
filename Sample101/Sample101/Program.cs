69fe178734a46595bd9e3623f45493af8b30024f
as <>
2021-01-11 14:21:50 +0900

    Dummy.cs、IFuncs1.cs、IFuncs2.csを作成

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,18 +6,7 @@ namespace Sample101
     {
         static void Main(string[] args)
         {
-            CellPhone cp = new CellPhone("hoge@email.con", "090-1244-5678");
-            //携帯電話クラスで、電話とメールを送る
-            cp.Call("011-123-4567");
-            cp.SendMail("fuga@email.com");
-            //電話インターフェイスでインスタンスにアクセス
-            IPhone phone = (IPhone)cp;
-            phone.Call("011-987-6543"); //電話をかける
-            // phone, SendMail("foo@email.com);     メールの送信メソッドは利用できない
-            //メールインターフェースでインスタンスにアクセス
-            IEmail mail = (IEmail)cp;
-            mail.SendMail("bar@email.com"); //メールを出す
-            //mail.Call("011-222-3333");    //mailインターフェースでは、電話の機能を利用できない
+            
         }
     }
 }
