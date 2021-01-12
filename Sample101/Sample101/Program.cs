34644bf8997e0875b4943d595f72f56d68e50bb8
as <>
2021-01-12 10:21:05 +0900

    例外処理について

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,35 +6,44 @@ using System.Threading.Tasks;
 
 namespace Sample101
 {
-    //Programクラス
     class Program
-    {   //デリゲートの宣言
-        delegate void Action(int a);
-        // １つ目の処理
-        static void Func1(int a)
-        {
-            Console.WriteLine("a={0}", a);
-        }
-        // 2つ目の処理
-        static void Func2(int a)
-        {
-            Console.WriteLine("a*2={0}", a * 2);
-        }
-        // 3つ目の処理
-        static void Func3(int a)
-        {
-            Console.WriteLine("a*3={0}", a * 3);
-        }
-
+    {
         static void Main(string[] args)
         {
-
-            Action a = new Action(Func1);
-            //処理の追加
-            a += new Action(Func2);
-            a += new Action(Func3);
-            //処理の実行
-            a(3);
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    int a = getNum(i);
+                    int b = 5;
+                    Console.Write(a + " / " + b + " = ");
+                    Console.WriteLine(calc(a, b));
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("0による割り算発生");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("配列の範囲外にアクセスしました");
+            }
+            finally
+            {
+                Console.WriteLine("終了");
+            }
+        }
+        //計算処理(例外を発生させる)
+        private static int calc(int a, int b)
+        {
+            return a / b;
+        }
+        //範囲外に出たときの処理
+        public static int getNum(int index)
+        {
+            int[] num = { 1, 2, 3, 4 };
+            return num[index];
         }
     }
 }
