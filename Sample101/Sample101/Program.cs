8560e0a4ea296bde023c928335ffadd463c1726f
as <>
2021-01-12 09:51:59 +0900

    複数のメソッドのデリゲートについて

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -6,32 +6,35 @@ using System.Threading.Tasks;
 
 namespace Sample101
 {
-    //デリゲート
-    delegate void Operation(int a, int b);
-    //Calcクラス
-    class Calc
-    {
-        public void Sub(int a, int b)
-        {
-            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
-        }
-    }
     //Programクラス
     class Program
-    {
-        static void Add(int a, int b)
+    {   //デリゲートの宣言
+        delegate void Action(int a);
+        // １つ目の処理
+        static void Func1(int a)
         {
-            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            Console.WriteLine("a={0}", a);
         }
+        // 2つ目の処理
+        static void Func2(int a)
+        {
+            Console.WriteLine("a*2={0}", a * 2);
+        }
+        // 3つ目の処理
+        static void Func3(int a)
+        {
+            Console.WriteLine("a*3={0}", a * 3);
+        }
+
         static void Main(string[] args)
         {
-            Calc c = new Calc();
-            //デリゲートの設定
-            Operation o1 = new Operation(Add);
-            Operation o2 = new Operation(c.Sub);
-            //デリゲートで設定したメソッドの呼び出し
-            o1(2, 1);
-            o2(2, 1);
+
+            Action a = new Action(Func1);
+            //処理の追加
+            a += new Action(Func2);
+            a += new Action(Func3);
+            //処理の実行
+            a(3);
         }
     }
 }
